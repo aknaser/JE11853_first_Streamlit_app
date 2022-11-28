32c10754e8c40f7838b503e5d220950e6a8f8bd9
aknaser <119278252+aknaser@users.noreply.github.com>
2022-11-28 20:15:00 +0400

    Update JE11853_first_Streamlit.py
    # Let's Create Some User Interaction
    # We'll add a user interactive widget called a Multi-select that will allow users to pick the fruits they want in their smoothies.
    # Let's put a pick list here so they can pick the fruit they want to include 
    
    streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
    
    # Display the table on the page.
    streamlit.dataframe(my_fruit_list)

--- JE11853_first_Streamlit.py
+++ JE11853_first_Streamlit.py
@@ -19,4 +19,13 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
+#streamlit.dataframe(my_fruit_list)
+# Let's Create Some User Interaction
+# We'll add a user interactive widget called a Multi-select that will allow users to pick the fruits they want in their smoothies.
+
+# Let's put a pick list here so they can pick the fruit they want to include 
+
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+
+# Display the table on the page.
 streamlit.dataframe(my_fruit_list)
