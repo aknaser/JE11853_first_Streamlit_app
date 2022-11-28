a53a7f9ebf69183c781e34ce54422ab22922af57
aknaser <119278252+aknaser@users.noreply.github.com>
2022-11-28 19:39:28 +0400

    Update JE11853_first_Streamlit.py
    # We're plan to use a CSV file from bucket in our Streamlit app. 
    # We want pandas to read CSV file from S3 bucket so we use a pandas function called "read_csv"  to pull the data into a dataframe we'll call my_fruit_list. 
    # Remember the S3 Bucket from DWW? 
    # We're going to use a CSV file from that bucket in our app. 
    # The file is here: https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt
    
    # We want pandas to read our CSV file from that S3 bucket so we use a pandas function called "read_csv"  to pull the data into a dataframe we'll call my_fruit_list. 
    
    # my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
    
    # After pulling the data into a pandas "dataframe" called "my_fruit_list", we will ask the streamlit library to display it on the page by typing:
    
    # streamlit.dataframe(my_fruit_list)

--- JE11853_first_Streamlit.py
+++ JE11853_first_Streamlit.py
@@ -1,4 +1,5 @@
 import streamlit
+import pandas
 
 streamlit.title('My Parents New Healthy Diner!')
 
@@ -10,3 +11,12 @@ streamlit.text('🐔Hard-Boiled Free-Range Egg')
 streamlit.text('🥑Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+## We want pandas to read our CSV file from that S3 bucket so we use a pandas function called read_csv  to pull the data into a dataframe we'll call my_fruit_list. 
+# Remember the S3 Bucket from DWW? 
+# We're going to use a CSV file from that bucket in our app. 
+# The file is here: https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
+streamlit.dataframe(my_fruit_list)
