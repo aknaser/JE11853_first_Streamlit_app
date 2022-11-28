bf8eef33f4bf27d0255ff1ee3f057f693f3f14d4
aknaser <119278252+aknaser@users.noreply.github.com>
2022-11-28 19:42:35 +0400

    Update JE11853_first_Streamlit.py
    Changed 'Breakfast Favorites:'  Menu to  streamlit.subheader() --- to see how it looks in streamlit.

--- JE11853_first_Streamlit.py
+++ JE11853_first_Streamlit.py
@@ -4,7 +4,7 @@ import pandas
 streamlit.title('My Parents New Healthy Diner!')
 
 streamlit.header('Breakfast Menu')
-streamlit.header('Breakfast Favorites:')
+streamlit.subheader('Breakfast Favorites:')
 streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥬Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔Hard-Boiled Free-Range Egg')
