659916f5b8313a7cdc995cb3145b979b2a27cf3b
aknaser <119278252+aknaser@users.noreply.github.com>
2022-12-01 15:21:44 +0400

    Update JE11853_first_Streamlit.py
    Comment Line#55. Not needed to display unstructured data

--- JE11853_first_Streamlit.py
+++ JE11853_first_Streamlit.py
@@ -52,7 +52,7 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header('Fryuityvice Fruit Advice!')
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+#streamlit.text(fruityvice_response.json())
 
 # Convert Unstructured data into normalized structured form 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
