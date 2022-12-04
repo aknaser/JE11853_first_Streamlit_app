2cf0913bbac2c1267ff1a8a122b363fb61b1959a
aknaser <119278252+aknaser@users.noreply.github.com>
2022-12-04 16:14:07 +0400

    Update JE11853_first_Streamlit.py
    Display User response

--- JE11853_first_Streamlit.py
+++ JE11853_first_Streamlit.py
@@ -79,4 +79,5 @@ streamlit.dataframe(my_data_rows)
 ####
 add_my_fruit = streamlit.text_input('What fruit would you like to Add?','jackfruit')
 streamlit.write('The user entered ', add_my_fruit)
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + add_my_fruit)
 streamlit.text("Thanks for adding, JacKfruit")
