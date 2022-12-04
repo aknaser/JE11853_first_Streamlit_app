95c78606faf4f6aeddbccf258cad2fb5318f1fa7
aknaser <119278252+aknaser@users.noreply.github.com>
2022-12-04 20:47:14 +0400

    Update JE11853_first_Streamlit.py
    First attempt...Fix the code to add the fruit Line#124

--- JE11853_first_Streamlit.py
+++ JE11853_first_Streamlit.py
@@ -121,7 +121,7 @@ if streamlit.button('Get Fruit Load List'):
 #Let's Use a Function and Button to Add the Fruit Name Submissions
 def insert_row_snowflake(new_fruit):
     with my_cnx.cursor() as my_cur:
-        my_cur.execute("insert into fruit_load_list values ('from streamlit')")
+        my_cur.execute("insert into fruit_load_list values ('" + new_fruit +"')")
         return "Thanks for adding, JacKfruit" + new_fruit
 add_my_fruit = streamlit.text_input('What fruit would you like to Add?')
 if streamlit.button('Add a Fruit to the List'):
